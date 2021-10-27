1cead909f4a9b1a7ebff29bba1507dca3ed84a85
jcga5673 <92279892+jcga5673@users.noreply.github.com>
2021-10-26 20:01:55 -0500

    Add files via upload

---
+++ Terraform-first-project/Project-01/main.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+  region = "us-east-2"
+}
+resource "aws_instance" "FirstServer" {
+  ami = "ami-00399ec92321828f5"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "ubuntu"
+  }
+}
